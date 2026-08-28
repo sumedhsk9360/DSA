f77e317fe95f650981c6d52243f96b9a45bf89db
sumedhsk9360 <sumedhsk14@gmail.com>
2026-08-29 03:03:29 +0530

    Time: 0 ms (100%), Space: 18.2 MB (8.29%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++) {
-            if(nums[i]==target) 
-            return true;
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) return true;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]) {
+                low++;
+                high--;
+            }
+            //left sorted
+            
+            else if(nums[low]<=nums[mid]) {
+                if(nums[low]<=target && target<=nums[mid]) {
+                   high=mid-1;
+                }
+                else {
+                   low=mid+1;
+                }
+            }
+            //right sorted
+            else {
+                if(nums[mid]<=target && target<=nums[high]) {
+                    low=mid+1;
+                }
+                else {
+                    high=mid-1;
+                }
+            }
         }
         return false;
     }
